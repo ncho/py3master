8e407eb377a29ff05d4ff86b0fd826a1fe5c262e
Nathan Cho <nat11374@esri.com>
2022-02-16 16:28:18 -0800

    ch 7 exercises and a missed ch 5 exercise

--- EXERCISES/set_sieve.py
+++ EXERCISES/set_sieve.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python
 
 # Exercise 7-4
+
+# Sets are useful when you want to keep track of a group of values,
+# but there's no particular value associated with them.
+
+# Import system package to use commands that interact with the system the program is being run on
+import sys
+
+# Checks to see if a limit is defined
+# Otherwise the default limit is 101
+if len(sys.argv) == 2:
+    limit = int(sys.argv[1])
+else:
+    limit = 101
+
+# Create set
+flags = set()
+
+print(2, end=' ')  # 2 is prime and does not need to be tested
+# range(start, stop, step)
+for num in range(3, limit, 2):  # Only test odd numbers, because all even numbers are non-prime
+    if num not in flags:
+        print(num, end=' ')
+        # Steps are being used to determine if a number is a multiple and thus non-prime
+        # For example, using 3 as the step will identify all numbers up to the limit that are divisible by 3
+        for x in range(num, limit, num):
+            flags.add(x)
+
+# This is a little different from sieve.py
+# The `if` conditional does the opposite of what it did before
--- EXERCISES/sieve.py
+++ EXERCISES/sieve.py
@@ -2,26 +2,67 @@
 
 # Exercises 5-4
 
+# The exercise's pseudo code and the answer are different from each other
+
 # Import system package to use commands that interact with the system the program is being run on
 import sys
 
-# The highest number to check
-# 101 is the default if no limit is specified on the command line or if the specified limit is ≤1
+# Specify highest number to check on the script's command line
+# A default is supplied if no limit is specified
 limit = 101
+# Check if a limit is specified on the command line
+# `len` checks number of inputs on the command line, not the size of a number
 if len(sys.argv) > 1:
-    limit = int(sys.argv[1]) + 1
+    if int(sys.argv[1]) > 1:
+        limit = int(sys.argv[1]) + 1
+    else:
+        print("A number greater than 1 must be entered.\n101 will be used as the limit instead.")
 
 # Initialize list
+# List is all Boolean values, set `True` by default
+# `True` is not a string
+# List is multiplied, replicates elements
+# Example: flags = [True] * 3 = [True, True, True]
+# Example: if limit = 5, then flags = [True, True, True, True, True]
 flags = [True] * limit
 
+print("The following are the prime numbers below {}:".format(limit - 1))
+
 # range(start, stop)
+# For every number between 2 and the limit
 for num in range(2, limit):
     if flags[num]:
+        # Index position is returned as number if value at index is `True` and prime
         print(num, end=' ')
+        # If a number is a multiple of another number, it is `False` and non-prime
         for multiple_of_num in range(2 * num, limit, num):
+            # `multiple_of_num` is the index position
+            # This calculates all the even numbers up to the limit and marks them as non-prime
             flags[multiple_of_num] = False
+
+print()
 
 # range(start, stop , step)
 
 # range (5, 30, 5):
 # Start = 5, stop = 30, step = 5 (5 through 25 by 5)
+
+# ---
+
+# Wikipedia pseudocode
+
+# A key thing is the numbers being outputted are index positions, not values
+
+# algorithm Sieve of Eratosthenes is
+#   input: an integer n > 1.
+#   output: all prime numbers from 2 through n.
+#
+#   let A be an array of Boolean values, indexed by integers 2 to n,
+#   initially all set to true.
+#
+#   for i = 2, 3, 4, ..., not exceeding √n do
+#       if A[i] is true
+#           for j = i², i²+i, i²+2i, i²+3i, ..., not exceeding n do
+#                A[j] := false
+
+# := can be used to assign variables while another expression is being evaluated.
